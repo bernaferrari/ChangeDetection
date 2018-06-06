399ce6cd73a907e2af04fa451230ec3f1d909594
Bernardo <bernaferrari2@gmail.com>
2018-06-06 02:08:49 -0300

    Now stripping <meta ...> too

--- app/src/main/java/com/bernaferrari/changedetection/extensions/StringExtensions.kt
+++ app/src/main/java/com/bernaferrari/changedetection/extensions/StringExtensions.kt
@@ -1,7 +1,7 @@
 package com.bernaferrari.changedetection.extensions
 
 fun String.cleanUpHtml(): String {
-    return this.replaceScriptTag().replaceLinkTag().replaceStyleTag()
+    return this.replaceScriptTag().replaceStyleTag().replaceLinkTag().replaceMetaTag()
 }
 
 // Avoid some pages from changing the script on every fetch
@@ -10,14 +10,20 @@ fun String.replaceScriptTag(): String {
     return this.replace("<script\\b[^<]*(?:(?!</script>)<[^<]*)*</script>".toRegex(), "")
 }
 
+// Avoid some pages from changing the style on every fetch
+// From https://stackoverflow.com/a/29888314/4418073
+fun String.replaceStyleTag(): String {
+    return this.replace("<style([\\s\\S]+?)</style>".toRegex(), "")
+}
+
 // Avoid some pages from changing the link on every fetch
 // From https://stackoverflow.com/a/7542023/4418073
 fun String.replaceLinkTag(): String {
     return this.replace("</?link(?:(?= )[^>]*)?>".toRegex(), "")
 }
 
-// Avoid some pages from changing the style on every fetch
+// Avoid some pages from changing the meta on every fetch
 // From https://stackoverflow.com/a/29888314/4418073
-fun String.replaceStyleTag(): String {
-    return this.replace("<style([\\s\\S]+?)</style>".toRegex(), "")
+fun String.replaceMetaTag(): String {
+    return this.replace("</?meta(?:(?= )[^>]*)?>".toRegex(), "")
 }
